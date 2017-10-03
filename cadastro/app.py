46fd70ae86cf5aa1aed3adf0719b50af3ecdcfc0
vitorrios1001 <vitorluizrios@hotmail.com>
2017-10-03 01:42:16 -0300

    Insert ready

--- cadastro/app.py
+++ cadastro/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, url_for, redirect
 
 from flask.ext.sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='Templates')
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/crud'
 
@@ -12,18 +12,47 @@ class Produto(db.Model):
     __tablename__='produto'
 
     _id = db.Column('id',db.Integer, primary_key=True, autoincrement=True)
-    nome = db.Column('nome',db.String(50))
+    desc = db.Column('desc',db.String(50))
+    qtdEstoque = db.Column('qtdEstoque',db.Integer)
+    vlr = db.Column('vlr',db.Float)
 
-    def __init__(self, nome):
-        self.nome = nome
+
+    def __init__(self, desc, qtdEstoque, vlr):
+        self.desc = desc
+        self.qtdEstoque = qtdEstoque
+        self.vlr = vlr
 
 db.create_all()
 
 @app.route("/")
 @app.route("/index")
-
 def index():
-    return "<h1>Ta funcinando</h1>"
+    return render_template("index.html")
+
+@app.route("/insert")
+def insert():
+    return render_template("insert.html")
+
+@app.route("/newInsert", methods=['GET','POST'])
+def newInsert():
+    if request.method == 'POST':
+        desc = (request.form.get("desc"))
+        qtdEstoque = (request.form.get("qtdEstoque"))
+        vlr = (request.form.get("vlr"))
+
+        if desc and qtdEstoque and vlr:
+            p = Produto(desc,qtdEstoque,vlr)
+            db.session.add(p)
+            db.session.commit()
+
+    return redirect(url_for("list"))
+
+@app.route("/list")
+def list():
+    produto = Produto.query.all()
+
+    return render_template("list.html", produtos=produto)
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
